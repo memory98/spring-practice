cbd2af1bfa97ce9eefe28f7aa35e74d59532af7c
memory98 <asdlkjh252567@gmail.com>
2023-01-27 16:05:10 +0900

    hello spring #4

---
+++ hellospring/src/main/java/com/douzone/hellospring/controller/BoardController.java
@@ -0,0 +1,36 @@
+package com.douzone.hellospring.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 
+ * @RequestMapping 메소드 단독 매핑
+ *
+ */
+
+
+//@Controller
+@RequestMapping("/board/*")
+public class BoardController {
+	
+	@ResponseBody
+	@RequestMapping("/board/write")
+	public String write() {
+		return "BoardController.wirte()";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/board/view")
+	public String view(Long no) {
+		return "BoardController.view("+no+")";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/board/view/{no}")
+	public String view1(@PathVariable("no") Long no) {
+		return "BoardController.view("+no+")";
+	}
+	
+}
