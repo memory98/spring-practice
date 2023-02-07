47ddbdfa4c689a8bd92b5497ae352cdf42d3ba9f
memory98 <asdlkjh252567@gmail.com>
2023-02-07 20:41:20 +0900

    fileupload

--- hellospring/src/main/java/com/douzone/hellospring/controller/BoardController.java
+++ hellospring/src/main/java/com/douzone/hellospring/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.douzone.hellospring.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,25 +12,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 
 
-//@Controller
-@RequestMapping("/board/*")
+@Controller
 public class BoardController {
 	
 	@ResponseBody
 	@RequestMapping("/board/write")
 	public String write() {
-		return "BoardController.wirte()";
-	}
-	
-	@ResponseBody
-	@RequestMapping("/board/view")
-	public String view(Long no) {
-		return "BoardController.view("+no+")";
+		return "BoardController.write()";
 	}
 	
 	@ResponseBody
 	@RequestMapping("/board/view/{no}")
-	public String view1(@PathVariable("no") Long no) {
-		return "BoardController.view("+no+")";
+	public String view(@PathVariable("no") Long no) {
+		return "BoardController.view(" + no + ")";
 	}
+	
 }
